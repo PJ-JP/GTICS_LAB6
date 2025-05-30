cce5d5063b26ad7a6b87371240154215c7371e81
PJ-JP <a20213805@pucp.edu.pe>
2025-05-29 20:40:29 -0500

    Commit

--- src/main/java/com/pucp/demo/entity/BlogPost.java
+++ src/main/java/com/pucp/demo/entity/BlogPost.java
@@ -1,4 +1,33 @@
 package com.pucp.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "blog_post")
+@Getter
+@Setter
 public class BlogPost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "titulo", nullable = false, length = 15)
+    private String titulo;
+
+    @Column(name = "contenido")
+    private String contenido;
+
+    @Column(name = "autor")
+    private String autor;
+
+    @Column(name="fecha_publicacion")
+    private String fechaPublicacion;
+
+    @Column(name="categoria")
+    private String categoria;
+
 }
